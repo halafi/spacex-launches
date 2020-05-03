7668503eb371786e07b3c0ee176e857c3452a6b4
halafi <filiphalas74@gmail.com>
2020-05-03 16:20:38 +0800

    build: setup gh pages

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# SpaceX launches
+# SpaceX launch list
 
-React application that will consume data from the SpaceX public API.
-The application must retrieve historic and future SpaceX launches and present them in a list view.
 
-Designs: [https://sketch.cloud/s/yyv1b/agmoaZP](https://sketch.cloud/s/yyv1b/agmoaZP)
-
-The API documentation is available from:
-Postman - [https://docs.spacexdata.com](https://docs.spacexdata.com/) 
-GitHub - [https://github.com/r-spacex/SpaceX-API](https://github.com/r-spacex/SpaceX-API)
-
-Stories:
-
-- As a user, I want the ability to load the full list of SpaceX launches from the SpaceX API
-- As a user, I want the ability to reload the data to see any new changes
-- As a user, I want the ability to filter the launch list by year
-- As a user, I want the ability to sort all launches by date (ascending/descending)
-
-Considerations we will be looking for in your submission are:
-
-- Project approach
-- Accuracy of designs
-- State management
-- Code quality
-- Network logic
-
-Please complete the task using a Git source control tool such as GitHub or Bitbucket. Remember to commit your code regularly. And as your submission, please provide access and the URL to the repository.
---
+++ docs/precache-manifest.57258d582feb9214b2019a9db4da9bd1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6a7d7d1ce259d13080334a851e9c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d673d6176507ac38b3",
+    "url": "/static/css/main.691722ae.chunk.css"
+  },
+  {
+    "revision": "a369ce4732d554140fc8",
+    "url": "/static/js/2.a743dc12.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a743dc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d673d6176507ac38b3",
+    "url": "/static/js/main.4af42d47.chunk.js"
+  },
+  {
+    "revision": "433ce4bea76e85f9c463",
+    "url": "/static/js/runtime-main.2284f8c5.js"
+  },
+  {
+    "revision": "1fa4147b0544c87a8933322999a887c5",
+    "url": "/static/media/BrandonGrotesque-Black.1fa4147b.woff2"
+  },
+  {
+    "revision": "33bc2fdf69d61c0abd8f43d41c995e87",
+    "url": "/static/media/BrandonGrotesque-Black.33bc2fdf.woff"
+  },
+  {
+    "revision": "3bebd53c0ee89703d8829e97e2ca0784",
+    "url": "/static/media/BrandonGrotesque-Black.3bebd53c.ttf"
+  },
+  {
+    "revision": "0b59275f161f6f43f0ca3b2365a77f77",
+    "url": "/static/media/BrandonGrotesque-Bold.0b59275f.ttf"
+  },
+  {
+    "revision": "1ad8d40bebcdcc1f79f0e22ce35b3d75",
+    "url": "/static/media/BrandonGrotesque-Bold.1ad8d40b.woff"
+  },
+  {
+    "revision": "441b33aac91c0f5883e390134d6fa6eb",
+    "url": "/static/media/BrandonGrotesque-Bold.441b33aa.woff2"
+  },
+  {
+    "revision": "08ee9db6dc65093bb66dbaae7094f087",
+    "url": "/static/media/BrandonGrotesque-Medium.08ee9db6.woff2"
+  },
+  {
+    "revision": "920c11f315161dc7ff0204c5502105e2",
+    "url": "/static/media/BrandonGrotesque-Medium.920c11f3.woff"
+  },
+  {
+    "revision": "ef4fddb0ac2f5cdf0b2eb54bf9898fb0",
+    "url": "/static/media/BrandonGrotesque-Medium.ef4fddb0.ttf"
+  },
+  {
+    "revision": "013a89eb356fb88baad2f6178c5400ba",
+    "url": "/static/media/BrandonGrotesque-Regular.013a89eb.woff"
+  },
+  {
+    "revision": "aba7d12553de107511905eb7595a72d9",
+    "url": "/static/media/BrandonGrotesque-Regular.aba7d125.ttf"
+  },
+  {
+    "revision": "aff015dfe96d4c9021ace79558be225d",
+    "url": "/static/media/BrandonGrotesque-Regular.aff015df.woff2"
+  },
+  {
+    "revision": "10cc8d8755a6ebdc69c8e4fa006de6fc",
+    "url": "/static/media/BrandonGrotesque-RegularItalic.10cc8d87.woff"
+  },
+  {
+    "revision": "5267a22825e445ebb40686639b9c1cd8",
+    "url": "/static/media/BrandonGrotesque-RegularItalic.5267a228.woff2"
+  },
+  {
+    "revision": "ba6abfddb0c1143b43d41ce9bc6fc236",
+    "url": "/static/media/BrandonGrotesque-RegularItalic.ba6abfdd.ttf"
+  },
+  {
+    "revision": "23a1b6b898599bb6c747d800291f4537",
+    "url": "/static/media/launch-home.23a1b6b8.png"
+  },
+  {
+    "revision": "b7cae1c6d33ed49312f7553a33380af7",
+    "url": "/static/media/launch-home@2x.b7cae1c6.png"
+  },
+  {
+    "revision": "59668b510e3b4413e6865a5f5ddf1d42",
+    "url": "/static/media/launch-home@3x.59668b51.png"
+  },
+  {
+    "revision": "950ba66541181838b6c07f14d62e331a",
+    "url": "/static/media/spacex-logo.950ba665.png"
+  }
+]);
